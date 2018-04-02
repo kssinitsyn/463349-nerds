00215d182cef735b33b71acfaf9faf383a3b3bb8
Kirill Sinitsyn <k.s.sinitsyn@gmail.com>
2018-04-02 21:12:58 +0300

    Добавлена анимация открытия и ошибки, в JS все подправлено, не работает пока цвет валидации и разфокус инпутов

--- js/modal.js
+++ js/modal.js
@@ -4,52 +4,42 @@ var popup = document.querySelector(".modal");
 
 var close = popup.querySelector(".button-close");
 
+var form = popup.querySelector("form");
+
 var login = popup.querySelector("[name=login]");
 
 var email = popup.querySelector("[name=email]");
 
-var textMessage = popup.querySelector("[name=textMessage]");
+var message = popup.querySelector("[name=message]");
 
-var isStorageSupport = true;
-var storage = "";
-
-try {
-  storage = localStorage.getItem("email");
-} catch (err) {
-  isStorageSupport = false;
-}
-
-link.addEventListener("click", function(evt) {
+link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
-  if (storage) {
-    email.value = storage;
-  }
-
   login.focus();
 });
 
-close.addEventListener("click", function(evt) {
+close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
 });
 
-// form.addEventListener("submit", function (evt) {
-//   if (!login.value || !email.value || !textMessage.value) {
-//     evt.preventDefault();
-//     console.log("Нужно ввести логин и пароль");
-//   } else {
-//     if (isStorageSupport) {
-//       localStorage.setItem("email", email.value);
-//     }
-//   }
-// });
-
-  window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (popup.classList.contains("modal-show")) {
-        popup.classList.remove("modal-show");
-      }
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+      
     }
-  });
+  }
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!login.value || !email.value || !message.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } 
+});
